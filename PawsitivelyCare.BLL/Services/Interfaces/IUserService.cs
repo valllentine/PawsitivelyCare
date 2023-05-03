5f7862acd41a6b1b1576aef128b668afa8b1223e
Valentyna Hrynyk <matilda20020216@gmail.com>
2023-05-03 21:50:05 +0300

    Add user service

---
+++ PawsitivelyCare.BLL/Services/Interfaces/IUserService.cs
@@ -0,0 +1,17 @@
+﻿using PawsitivelyCare.BLL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawsitivelyCare.BLL.Services.Interfaces
+{
+    internal interface IUserService
+    {
+        Task<UserModel> Add(UserModel user);
+        Task<UserModel> Get(int id);
+        Task Update(UserModel user);
+        Task Delete(Guid id);
+    }
+}
---
+++ PawsitivelyCare.BLL/Services/Realizations/UserService.cs
@@ -0,0 +1,57 @@
+﻿using AutoMapper;
+using PawsitivelyCare.BLL.Models;
+using PawsitivelyCare.BLL.Services.Interfaces;
+using PawsitivelyCare.DAL.Entities;
+using PawsitivelyCare.DAL.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawsitivelyCare.BLL.Services.Realizations
+{
+    internal class UserService : IUserService
+    {
+        private readonly IBaseRepository<User, int> _userRepository;
+        protected readonly IMapper _mapper;
+
+        public UserService(IBaseRepository<User, int> userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<UserModel> Add(UserModel address)
+        {
+            var entity = _mapper.Map<User>(address);
+            var createdEntity = await _userRepository.AddAsync(entity);
+
+            return _mapper.Map<UserModel>(createdEntity);
+        }
+
+        public async Task<UserModel> Get(int id)
+        {
+            var User = await _userRepository.GetAsync(id);
+
+            return _mapper.Map<UserModel>(User);
+        }
+
+        public async Task Update(UserModel userModel)
+        {
+            var userEntity = _mapper.Map<User>(userModel);
+
+            await _userRepository.UpdateAsync(userEntity);
+        }
+
+        public async Task Delete(Guid id)
+        {
+            var userEntity = await _userRepository.GetAsync(id);
+
+            if (userEntity == null)
+                throw new ArgumentException($"User with the id '{id}' could not be found.");
+
+            await _userRepository.DeleteAsync(userEntity);
+        }
+    }
+}
