a00da638684e19e5c1261066c90ceac1e22bac08
Valentyna Hrynyk <matilda20020216@gmail.com>
2023-05-03 21:49:34 +0300

    Add models

---
+++ PawsitivelyCare.BLL/Models/ChatMessageModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PawsitivelyCare.BLL.Models
+{
+    internal class ChatMessageModel
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int ChatId { get; set; }
+        public int SenderId { get; set; }
+    }
+}
