fcf23c31ab47869e36f77e088cdf58bdc4394122
Valentyna Hrynyk <matilda20020216@gmail.com>
2023-05-03 23:01:41 +0300

    Configure program class

--- PawsitivelyCare/Program.cs
+++ PawsitivelyCare/Program.cs
@@ -1,31 +1,44 @@
 using Microsoft.EntityFrameworkCore;
+using PawsitivelyCare.BLL.Services.Interfaces;
+using PawsitivelyCare.BLL.Services.Realizations;
 using PawsitivelyCare.DAL.Contexts;
+using PawsitivelyCare.DAL.Entities;
+using PawsitivelyCare.DAL.Repositories.Interfaces;
+using PawsitivelyCare.DAL.Repositories.Realizations;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddDbContext<PawsitivelyCareDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("PawsitivelyCareDBConnection")));
+//builder.Services.AddDbContext<PawsitivelyCareDbContext>(options =>
+//    options.UseSqlServer(builder.Configuration.GetConnectionString("PawsitivelyCareDBConnection")));
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+builder.Services.AddDbContextFactory<PawsitivelyCareDbContext>(
+            options => options.UseSqlServer($"name={builder.Configuration.GetConnectionString("PawsitivelyCareDBConnection")}"));
+
+builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddScoped<IBaseRepository<User, int>, BaseRepository<User, int>>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+builder.Services.AddAuthorization();
+builder.Services.AddControllers();
 
 var app = builder.Build();
+app.UseSwaggerUI();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+app.UseSwagger(x => x.SerializeAsV2 = true);
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
 
 app.UseAuthorization();
-
-app.MapRazorPages();
 
 app.Run();
