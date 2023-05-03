ebff7a15a65899d7865e9a173289b0e4c9cd0826
Valentyna Hrynyk <matilda20020216@gmail.com>
2023-05-03 23:00:49 +0300

    Delete unused fields in the models

--- PawsitivelyCare.BLL/Models/ChatModel.cs
+++ PawsitivelyCare.BLL/Models/ChatModel.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PawsitivelyCare.BLL.Models
+﻿namespace PawsitivelyCare.BLL.Models
 {
-    internal class ChatModel
+    public class ChatModel
     {
         public int Id { get; set; }
         public string Name { get; set; }
--- PawsitivelyCare.BLL/Models/PostModel.cs
+++ PawsitivelyCare.BLL/Models/PostModel.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PawsitivelyCare.BLL.Models
+﻿namespace PawsitivelyCare.BLL.Models
 {
-    internal class PostModel
+    public class PostModel
     {
         public int Id { get; set; }
         public string Title { get; set; }
