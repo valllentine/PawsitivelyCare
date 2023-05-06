44c94775730acf7b0d76b209e63d6c84b6050b07
Valentyna Hrynyk <matilda20020216@gmail.com>
2023-05-06 19:57:03 +0300

    Edit id type in models

--- PawsitivelyCare.BLL/Models/ChatMessageModel.cs
+++ PawsitivelyCare.BLL/Models/ChatMessageModel.cs
@@ -2,10 +2,10 @@
 {
     public class ChatMessageModel
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
         public string Text { get; set; }
         public DateTime CreatedAt { get; set; }
-        public int ChatId { get; set; }
-        public int SenderId { get; set; }
+        public Guid ChatId { get; set; }
+        public Guid SenderId { get; set; }
     }
 }
