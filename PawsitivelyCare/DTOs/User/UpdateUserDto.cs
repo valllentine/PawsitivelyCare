9f909395fd985460cc4f70dbc43d44feb1ff39fc
Valentyna Hrynyk <matilda20020216@gmail.com>
2023-05-06 19:55:25 +0300

    Add user dto

---
+++ PawsitivelyCare/DTOs/User/UpdateUserDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PawsitivelyCare.DTOs.User
+{
+    public class UpdateUserDto
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
