5c5f930816ad487e7011c47c625bcd4c0f56ca31
Valentyna Hrynyk <matilda20020216@gmail.com>
2023-05-07 14:22:18 +0300

    Edit user service

--- PawsitivelyCare.BLL/Services/Interfaces/IUserService.cs
+++ PawsitivelyCare.BLL/Services/Interfaces/IUserService.cs
@@ -5,8 +5,9 @@ namespace PawsitivelyCare.BLL.Services.Interfaces
     public interface IUserService
     {
         Task<UserModel> Register(UserModel user);
+        Task<string> Login(UserModel userModel);
         Task<UserModel> Get(Guid id);
-        Task Update(UserModel user);
+        Task Update(UserModel userModel);
         Task Delete(Guid id);
     }
 }
--- PawsitivelyCare.BLL/Services/Realizations/UserService.cs
+++ PawsitivelyCare.BLL/Services/Realizations/UserService.cs
@@ -5,6 +5,11 @@ using PawsitivelyCare.BLL.Models;
 using PawsitivelyCare.BLL.Services.Interfaces;
 using PawsitivelyCare.DAL.Entities;
 using PawsitivelyCare.DAL.Repositories.Interfaces;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace PawsitivelyCare.BLL.Services.Realizations
 {
@@ -12,21 +17,35 @@ namespace PawsitivelyCare.BLL.Services.Realizations
     {
         private readonly IBaseRepository<User, Guid> _userRepository;
         protected readonly IMapper _mapper;
+        private readonly IConfiguration _configuration;
 
-        public UserService(IBaseRepository<User, Guid> userRepository, IMapper mapper)
+        public UserService(IBaseRepository<User, Guid> userRepository, IMapper mapper, IConfiguration configuration)
         {
             _userRepository = userRepository;
             _mapper = mapper;
+            _configuration = configuration;
+        }
+
+        public async Task<string> Login(UserModel userModel)
+        {
+            var userEntity = await _userRepository.QueryFirst(u => u.Email == userModel.Email);
+
+            if (userEntity == null || !(await PasswordVerify(userModel.Password, userEntity.PasswordHash)))
+                throw new ArgumentException("Username or password is incorrect");
+
+            string token = CreateToken(userModel);
+
+            return token;
         }
 
         public async Task<UserModel> Register(UserModel model)
         {
-            if (_userRepository.QueryFirst(u=>u.Email == model.Email) != null)
+            if (await _userRepository.QueryFirst(u=>u.Email == model.Email) != null)
                 throw new ArgumentException("Username with email " + model.Email + " already exist");
 
             var userEntity = _mapper.Map<User>(model);
 
-            userEntity.PasswordHash = BCrypt.Net.BCrypt.HashPassword(model.Password);
+            userEntity.PasswordHash = await HashPasswordAsync(model.Password);
             userEntity.CreatedAt= DateTime.Now;
 
             var createdEntity = await _userRepository.AddAsync(userEntity);
@@ -43,7 +62,13 @@ namespace PawsitivelyCare.BLL.Services.Realizations
 
         public async Task Update(UserModel userModel)
         {
+            if (await _userRepository.QueryFirst(u => u.Email == userModel.Email) != null)
+                throw new ArgumentException("Email '" + userModel.Email + "'  already exist");
+
             var userEntity = _mapper.Map<User>(userModel);
+
+            if (!string.IsNullOrEmpty(userModel.Password))
+                userEntity.PasswordHash = await HashPasswordAsync(userModel.Password);
 
             await _userRepository.UpdateAsync(userEntity);
         }
@@ -56,6 +81,39 @@ namespace PawsitivelyCare.BLL.Services.Realizations
                 throw new ArgumentException($"User with the id '{id}' could not be found.");
 
             await _userRepository.DeleteAsync(userEntity);
+        }
+
+        private string CreateToken(UserModel userModel)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, userModel.Email)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
+                _configuration.GetSection("AppSettings:Token").Value!));
+
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(5),
+                signingCredentials: credentials
+                );
+
+            var jwtToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwtToken;
+        }
+
+        private async Task<string> HashPasswordAsync(string password)
+        {
+            return await Task.Run(() => BCrypt.Net.BCrypt.HashPassword(password));
+        }
+
+        private async Task<bool> PasswordVerify(string password, string passwordHash)
+        {
+            return await Task.Run(() => BCrypt.Net.BCrypt.Verify(password, passwordHash));
         }
     }
 }
